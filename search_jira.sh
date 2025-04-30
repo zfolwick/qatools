88e1163287238b9c551594d1337903d649c09b01
zfolwick <zfolwick@gmail.com>
2025-04-29 22:38:20 -0700

    added and updated files

--- search_jira.sh
+++ search_jira.sh
@@ -1,31 +1,34 @@
 #!/usr/bin/env bash
-usage() {
+[[ -z "${JIRA_TOKEN}" ]] && echo "JIRA_TOKEN environment variable is not set. Please set the JIRA_TOKEN" && exit 1
+
+jira_usage() {
   echo "jira query tool
   > search jira --query=TERM --type=[Bug,Story,Task,Epic]"
 }
 
-help() {
-  
-  usage
+jira_help() {
+  jira_usage
   echo "
- -q,--query       single word jira term to query
- -t,--type        the issue type (Bug,Story,Task,Epic)
- -h,--help        this help screen
- -p,--pretty      formats verbose output for jq
- -v,--verbose     displays the URL search query
- -vvv             displays the whole contents of jira, unfiltered
+  -q|--query       single word jira term to query
+  -t|--type        the issue type (Bug,Story,Task,Epic)
+  -b|--bugs        displays associated bugs
+  -r|--raw         displays unfiltered, raw json response
+  -v|--verbose     display the query url
+  -p|--pretty      format the response using jq
+  -h|--help        display this menu
+  -vvv             display the whole response
   "
   exit $1
 }
 
 search_jira() {
   if [[ "$@" =~ "-h" || "$@" =~ "--help" ]]; then
-    help 0
+    jira_help 0
   fi
 
-  [[ $# -lt 2 ]] && echo "need to pass in a search query and jira ticket type." && usage
+  [[ $# -lt 2 ]] && echo "need to pass in a search query and jira ticket type." && jira_usage
 
-  if ! [[ "$@" =~ "query="[A-Z]+ && "$@" =~ "type="[Bug|Story|Epic|Task] ]]; then
+  if ! [[ "$@" =~ "query="[A-Z|a-z|0-9]+ && "$@" =~ "type="[Bug|Story|Epic|Task] ]]; then
     echo $@
     echo "need to pass in a search query and jira ticket type." && exit 1
   fi
@@ -35,6 +38,8 @@ search_jira() {
   local echo_verbose=false
   local pretty=false
   local echo_query_url=false
+  local raw=false
+  local show_associated_bugs=false
 
   until test $# -eq 0  ; do
     case "$1" in
@@ -48,13 +53,23 @@ search_jira() {
         shift
         continue
         ;;
+      -b|--bugs)
+        show_associated_bugs=true
+        shift
+        continue
+        ;;
       -p|--pretty)
         pretty=true
         shift
         continue
         ;;
+      -r|--raw)
+        raw=true
+        shift
+        continue
+        ;;
       -h|--help)
-        help 0
+        jira_help 0
         ;;
       -v|--verbose)
         echo_query_url=true
@@ -67,30 +82,31 @@ search_jira() {
         continue
         ;;
       *)
-        help 1
+        jira_help 1
     esac
 
     if ! [[ -z $type && -z $query ]]; then break; fi
   done
 
-  if [[ $type_string =~ "--type" ]]; then
-    echo "type not set" 
-    exit 1
-  fi
-
-  if [[ $query_string =~ "--query" ]]; then
-    echo "query not set" 
-    exit 1
-  fi
-
   if [ -z $type_string ] || [ -z $query_string ]; then echo "query or type not set" && echo "query: ${query_string}; type: ${type_string}" && exit 1 ; fi
 
-  project_text="project%20%3D%20AOTA%20"
-  query_url="${JIRA_URL}?jql=${project_text}AND%20text%20~%20${query_string}%20AND%20issuetype=${type_string}&fields=key"
+  project_text=$JIRA_PROJECT_TEXT
+  query_url="$JIRA_URL$JIRA_ENDPOINT?jql=${project_text}AND%20text%20~%20${query_string}%20AND%20issuetype=${type_string}&fields=key"
+
+
+  if $echo_query_url; then
+    echo $query_url
+  fi
 
   response=$(curl -s --request GET $query_url -H 'Authorization: Bearer '$JIRA_TOKEN) 
 
+  echo $response
+
   [[ $response =~ "Error" ]] && echo "something wrong with the query" && echo $response
+
+  if $raw; then
+    echo $response
+  fi
 
   if $echo_verbose; then
     echo $query_url
@@ -103,14 +119,12 @@ search_jira() {
     exit 0
   fi
 
-  associated_bugs=$(echo $response | jq -r '.issues[] | .key' | xargs)
-
-  if $echo_query_url; then
-    echo $query_url
+  if $show_associated_bugs; then
+    associated_bugs=$(echo $response | jq -r '.issues[] | .key' | xargs)
     echo $associated_bugs
     exit 0
   fi
 
-  echo $associated_bugs
+  echo $response
 }
 
---
+++ search_testrail.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+[ -z "$TESTRAIL_USERNAME" ] && echo add TESTRAIL_USERNAME to .qaconfig file && exit 1
+[ -z "$TESTRAIL_PASSWORD" ] && echo add TESTRAIL_PASSWORD to .qaconfig file && exit 1
+[ -z "$TESTRAIL_URL" ] && echo add TESTRAIL_URL to .qaconfig file && exit 1
+
+testrail_usage() {
+  echo "search testrail - by id
+>./search.sh testrail -c|--case=TEST_CASE_ID [--verbose|-vvv|--pretty|--raw|--help]
+"
+}
+
+help() {
+  testrail_usage
+
+  echo "
+  -c|--case        the test case id to query
+  -r|--raw         displays unfiltered, raw json response
+  -v|--verbose     display the query url
+  -p|--pretty      format the response using jq
+  -h|--help        display this menu
+  -vvv             display the whole response
+  "
+  exit 1
+}
+
+search_testrail() {
+  [ -z $1 ] && echo "add a test case id" && testrail_usage && exit 1
+
+  local echo_verbose=false
+  local pretty=false
+  local echo_query_url=false
+  local query=""
+  local raw=false
+  until test $# -eq 0  ; do
+    case $1 in
+      -c*|--case*)
+        id=$1
+        query="${id#*=}"
+        shift
+        ;;
+      -v|--verbose) 
+        echo_query_url=true
+        shift
+        ;;
+      -vvv)
+        echo_verbose=true
+        shift
+        ;;
+      -p|--pretty)
+        pretty=true
+        shift
+        ;;
+      -r|--raw)
+        raw=true
+        shift
+        continue
+        ;;
+      -h|--help)
+        help 0
+      ;;
+      *)
+        help 1
+    esac
+  done
+  [[ -z $query ]] && echo "test case id not set" && help 1
+
+  query_url="$TESTRAIL_URL?$TESTRAIL_ENDPOINT$query"
+
+  response=$(curl -s --request GET $query_url -H "Content-Type: application/json" -u "$TESTRAIL_USERNAME:$TESTRAIL_PASSWORD")
+
+  [[ $response =~ "Error" ]] && echo "something wrong with the query" && echo $response
+
+  if $echo_verbose; then
+    echo $query_url
+    echo $response 
+    exit 0
+  fi
+
+  if $echo_query_url; then
+    echo $query_url
+    echo $response | jq '{"priority_id", "suite_id"}'
+    exit 0
+  fi
+
+  if $pretty; then
+    jq_pretty_script="$TESTRAIL_PRETTY_FILTER"
+    echo $response | jq $jq_pretty_script
+    exit 0
+  fi
+
+  if $raw; then
+    echo $response
+  fi
+}
+
